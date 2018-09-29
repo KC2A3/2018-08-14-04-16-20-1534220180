26dc3f152a81aee2e6f20314d5f114a9d2af3367
KC2A3 <947496351@qq.com>
2018-09-29 15:35:32 +0800

    第n次修改

--- main/main.js
+++ main/main.js
@@ -1,18 +1,21 @@
-module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
-};
+function newplan(sequence){
+   var sequence=[6, 9, 15, -2, 92, 11];
+   var maxnum = 0;
+    for (var i = 0; i < sequence.length; i++) {
+      sequence[i]>maxnum ? maxnum =sequence[i] : null;
+    }
+    var minnum = 0;
+    for (var i = 0; i < sequence.length; i++) {
+      sequence[i]<minnum ? minnum =sequence[i] : null;
+    }
+    var average = 0;
+    for (var i = 0; i < sequence.length; i++) {
+      average += sequence[i];
+    }
+    var A = ~~(average/sequence.length*100)/100;
+    var B = sequence.length
+  
+  console.log('\no) 最小值 = '+minnum+'\n'+'o) 最大值 = '+maxnum+'\n'+'o) 元素数量 = '+B+'\n'+'o) 平均值 = '+A)
 
-class Sequence {
-  constructor(input) {
-    // Write your code here
   }
-
-  minimum() {
-    // Write your code here
-  }
-
-  // Write your code here
-}
+  module.exports = {newplan}
